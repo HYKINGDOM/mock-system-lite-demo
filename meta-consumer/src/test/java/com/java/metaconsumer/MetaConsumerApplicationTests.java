9a4d6fa9a232506d3b6ad6fef4138faa7e4cc1da
yihurCoder <1370054591@qq.com>
2022-11-09 22:42:06 +0800

    first commit

---
+++ meta-consumer/src/test/java/com/java/metaconsumer/MetaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.metaconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
