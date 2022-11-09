28ebe9a799507cc2ed6f4ad6f92bc7d0ab5e3ccf
yihurCoder <1370054591@qq.com>
2022-11-09 22:45:37 +0800

    first commit

---
+++ meta-admin/src/main/java/com/java/metaadmin/MetaAdminApplication.java
@@ -0,0 +1,13 @@
+package com.java.metaadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetaAdminApplication.class, args);
+    }
+
+}
---
+++ meta-consumer/src/main/java/com/java/metaconsumer/MetaConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.java.metaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetaConsumerApplication.class, args);
+    }
+
+}
