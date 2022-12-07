0c3abf85874134c437d191c7bb6afba829977b84
yihurCoder <1370054591@qq.com>
2022-12-07 22:53:54 +0800

    build project file

---
+++ meta-crm/src/main/java/com/java/project/meta/crm/MetaCrmApplication.java
@@ -0,0 +1,13 @@
+package com.java.project.meta.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetaCrmApplication.class, args);
+    }
+
+}
